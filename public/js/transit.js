579621e5ea1c7fc03c0b8582279bce2520e42ea8
Arturo Perez <VrturoPerez@gmail.com>
2015-12-09 17:07:59 -0800

    add transit get data button, missing header

--- public/js/transit.js
+++ public/js/transit.js
@@ -1,12 +1,24 @@
+var transitHeaderUrl = "";
+var transitHeader = $('#transitHeader').on('click', function(e){
+      e.preventDefault();
+      var cb = function(responseData){
+        console.log(responseData)
+      };
+      getTransitData(transitHeaderUrl, 'GET', null, cb);
+  });
 
+
+
+
+
+var transitUrl = "http://services.my511.org/Transit2.0/GetStopsForRoutes.aspx?token=#{ ENV['TRANSIT_API_KEY'] }&routeIDF=WestCAT~10~LOOP|BART~917"
 var transitInit = $('#transit').on('click', function(e){
       e.preventDefault();
       var cb = function(responseData){
         console.log(responseData)
       };
-      getTransitData(transit_api_url, 'GET', null, cb);
+      getTransitData(transitUrl, 'GET', null, cb);
   });
-
 
 var getTransitData = function(url, method, data, callback){
   $.ajax({
