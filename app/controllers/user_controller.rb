520597ae3d754e259894d325dace3785b87e3e16
Arturo Perez <VrturoPerez@gmail.com>
2015-12-09 23:19:28 -0800

    get response data to show up client side

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,10 @@
 get '/' do
 
-  p @response = HTTParty.get('http://services.my511.org/Transit2.0/GetAgencies.aspx?token=58cb6407-ebd8-4d7d-894c-27da1e56e7e3')
+  @hello = "hello there"
+
+  response = HTTParty.get('http://services.my511.org/Transit2.0/GetAgencies.aspx?token=58cb6407-ebd8-4d7d-894c-27da1e56e7e3')
+  @data = response
+
+
   erb :index
 end
