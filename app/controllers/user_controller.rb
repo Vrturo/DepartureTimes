7d526c3d141a96c90dd28f1485524ea2c1ba7a16
Arturo Perez <VrturoPerez@gmail.com>
2015-12-16 19:46:43 -0800

    enter stops and get 404

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,11 +4,25 @@ get '/' do
   agency = HTTParty.get(agencylink)
   agency_arr = []
   agency["RTT"]["AgencyList"]["Agency"].each do |key|
-    if key["Mode"] == "Rail"
+    if key["Name"] == "BART"
       agency_arr << key
     end
   end
   @agency_arr = agency_arr
+
+  routeforagencylink = "http://services.my511.org/Transit2.0/GetRoutesForAgency.aspx?token=#{ENV['TRANSIT_API_KEY']}&agencyName=BART"
+  @routeagency = HTTParty.get(routeforagencylink)
+
+  getnextdeplink = "http://services.my511.org/Transit2.0/GetNextDeparturesForStopName.aspx?token=#{ENV['TRANSIT_API_KEY']}&agencyName=BART&Name=Dublin/Pleasanton"
+  @getnextdep = HTTParty.get(getnextdeplink)
+
+
+
+
+
+
+
+
 
 
   stopsLink = "http://services.my511.org/Transit2.0/GetStopsForRoute.aspx?routeIDF=Caltrain~LOCAL~SB1&token=#{ENV['TRANSIT_API_KEY']}"
@@ -23,7 +37,7 @@ get '/' do
   @transit_arr = transit_arr
   @stop_arr = stop_arr
 
-  getDepartureLink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=#{ENV['TRANSIT_API_KEY']}&stopcode=#{@stop_arr[1]}"
+  getDepartureLink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=#{ENV['TRANSIT_API_KEY']}&stopcode=#{@stop_arr[0]}"
   @departure = HTTParty.get(getDepartureLink)
 
   erb :index
