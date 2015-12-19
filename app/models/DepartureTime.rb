e7ada3a59dc17751e98a08465f6c2f971efc0f76
Arturo Perez <VrturoPerez@gmail.com>
2015-12-18 16:13:29 -0800

    get array of departure times

--- app/models/DepartureTime.rb
+++ app/models/DepartureTime.rb
@@ -1,19 +1,19 @@
 class DepartureTime
 
-  attr_accessor :departuretime
+  attr_accessor :departuretime, :departure
   def initialize(stopcode)
     @departuretime = []
     @stopcode = stopcode
     departuretimelink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=#{ENV['TRANSIT_API_KEY']}&stopcode=#{@stopcode}"
+    @departure = HTTParty.get(departuretimelink)
   end
 
   def get_stops
-    departure = HTTParty.get(departuretimelink)
-      departure["RTT"]["AgencyList"]["Agency"]["RouteList"]["Route"].each do |hash|
+      @departure["RTT"]["AgencyList"]["Agency"]["RouteList"]["Route"].each do |hash|
         if hash.has_key?("RouteDirectionList")
           if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"] != nil
-            hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].each do |time|
-              @departuretime << "Minutes till next Departure: " + time
+            hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].each do |time|
+                @departuretime << "Minutes till next Departure: " + time.flatten!.to_s
             end
           else
             next
@@ -22,5 +22,6 @@ class DepartureTime
           next
         end
       end
-      @departuretime
+      puts @departuretime
     end
+end
