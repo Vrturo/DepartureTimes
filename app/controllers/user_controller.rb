a27b353cefba72dffcec5b40497e3226258d242c
Arturo Perez <VrturoPerez@gmail.com>
2015-12-19 18:26:40 -0800

    create Bart model

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,14 +4,28 @@ get '/' do
   agency = HTTParty.get(agencylink)
   agency_arr = []
   agency["RTT"]["AgencyList"]["Agency"].each do |key|
-    if key["Name"] == "BART"
+    if key["HasDirection"] == "True"
       agency_arr << key
     end
   end
   @agency_arr = agency_arr
 
+
+
   routeforagencylink = "http://services.my511.org/Transit2.0/GetRoutesForAgency.aspx?token=#{ENV['TRANSIT_API_KEY']}&agencyName=BART"
   @routeagency = HTTParty.get(routeforagencylink)
+  route_name = []
+  route_code = []
+  @routeagency["RTT"]["AgencyList"]["Agency"]["RouteList"]["Route"].each do |key|
+    route_name << key["Name"]
+    route_code << key["Code"]
+  end
+  @route_name = route_name
+  @route_code = route_code
+
+  bartlink = "http://services.my511.org/Transit2.0/GetStopsForRoute.aspx?token=#{ENV['TRANSIT_API_KEY']}&routeIDF=BART~917"
+
+
 
 
   northboundlink = "http://services.my511.org/Transit2.0/GetStopsForRoute.aspx?routeIDF=Caltrain~LOCAL~NB&token=#{ENV['TRANSIT_API_KEY']}"
