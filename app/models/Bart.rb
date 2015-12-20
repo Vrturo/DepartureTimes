cb733fd9d2d9a91b09af1b75df68b2a662bb9a95
Arturo Perez <VrturoPerez@gmail.com>
2015-12-19 18:35:10 -0800

    bart has no direction code

--- app/models/Bart.rb
+++ app/models/Bart.rb
@@ -1,2 +1,14 @@
 class Bart
+
+ attr_accessor :departuretime
+  def initialize(routecode)
+    @departuretime = []
+    @routecode = routecode
+    departuretimelink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=#{ENV['TRANSIT_API_KEY']}&routeIDF=BART~#{@routecode}"
+    @departure = HTTParty.get(departuretimelink)
+  end
+
+  def get_stops
+    @departure
+  end
 end
