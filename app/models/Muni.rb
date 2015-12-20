e6779570c971c63f3d2c6643ef513bd1428db6df
Arturo Perez <VrturoPerez@gmail.com>
2015-12-19 19:42:52 -0800

    edit Muni model

--- app/models/Muni.rb
+++ app/models/Muni.rb
@@ -1,15 +1,26 @@
 class Muni
 
- attr_accessor :departuretime
+ attr_accessor :departuretime, :stopname, :stopcode
   def initialize(routecode, routedirectioncode)
     @departuretime = []
+    @stopname = []
+    @stopcode = []
     @routecode = routecode
     @routedirectioncode = routedirectioncode
     departuretimelink = "http://services.my511.org/Transit2.0//GetStopsForRoute.aspx?token=#{ENV['TRANSIT_API_KEY']}&routeIDF=SF-MUNI~#{@routecode}~#{@routedirectioncode}"
     @departure = HTTParty.get(departuretimelink)
   end
 
-  def get_stops
-    @departure
+  def get_stopcode_and_name
+    @departure["RTT"]["AgencyList"]["Agency"]["RouteList"]["Route"]["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"].each do |key|
+      @stopname << key["name"]
+      @stopcode << key["StopCode"]
+    end
   end
+
+  def show_stops
+    self.get_stopcode_and_name
+
+  end
+
 end
