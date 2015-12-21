b796791849b1504aca5a9a398caa964b56b1c9c3
Arturo Perez <VrturoPerez@gmail.com>
2015-12-21 14:38:20 -0800

    change cal train uri to by stopname instead of stopcode

--- app/models/CalTrain.rb
+++ app/models/CalTrain.rb
@@ -4,30 +4,32 @@ class CalTrain
   def initialize(stopcode)
     @departuretime = []
     @stopcode = stopcode
-    departuretimelink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=#{ENV['TRANSIT_API_KEY']}&stopcode=#{@stopcode}"
+    #departuretimelink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=#{ENV['TRANSIT_API_KEY']}&stopcode=#{@stopcode}"
+    departuretimelink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopName.aspx?token=#{ENV['TRANSIT_API_KEY']}&agencyName=Caltrain&stopName=#{@stopcode}"
     @departure = HTTParty.get(departuretimelink)
   end
 
   def get_stops
-      @departure["RTT"]["AgencyList"]["Agency"]["RouteList"]["Route"].each_with_index do |hash, index|
+      @departure#["RTT"]["AgencyList"]["Agency"]["RouteList"]["Route"].each_with_index do |hash, index|
 
-        if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].nil?
-          @departuretime << "No departures within the next hour"
-        else
-          if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].class == Array
-              hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].each do |time|
-                @departuretime << "Stop #{index + 1}: Minutes till next Departure: " + time
-              end
-            next
-          elsif hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].class == String
-              @departuretime << "Stop #{index + 1}: Minutes till next Departure: " + hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"]
-            next
-          else
-            @departuretime << "Stop #{index + 1}: No departures within the next hour"
-          end
-        end
+        # if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].nil?
+        #   @departuretime << "No departures within the next hour"
+        #   next
+        # else
+        #   if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].class == Array
+        #       hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].each do |time|
+        #         @departuretime << "Stop #{index + 1}: Minutes till next Departure: " + time
+        #       end
+        #     next
+        #   elsif hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].class == String
+        #       @departuretime << "Stop #{index + 1}: Minutes till next Departure: " + hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"]
+        #     next
+        #   else
+        #     @departuretime << "Stop #{index + 1}: No departures within the next hour"
+        #   end
+        # end
 
-        return @departuretime.join("<br>")
-    end #each
+        # return @departuretime.join("<br>")
+    # end #each
   end #method
 end #class
