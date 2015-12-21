1da228b3ec7cbfb31ed0c6885b4375cb9da64c9b
Arturo Perez <VrturoPerez@gmail.com>
2015-12-21 14:48:25 -0800

    add get stops by name method on caltrain model

--- app/models/CalTrain.rb
+++ app/models/CalTrain.rb
@@ -1,35 +1,43 @@
 class CalTrain
 
-  attr_accessor :departuretime
-  def initialize(stopcode)
+  attr_accessor :departuretime, :stopcode, :stopname
+  def initialize(stopname, stopcode)
     @departuretime = []
+    @stopname = stopname.delete(" ") #delete white space for html request
     @stopcode = stopcode
-    #departuretimelink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=#{ENV['TRANSIT_API_KEY']}&stopcode=#{@stopcode}"
-    departuretimelink = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopName.aspx?token=#{ENV['TRANSIT_API_KEY']}&agencyName=Caltrain&stopName=#{@stopcode}"
-    @departure = HTTParty.get(departuretimelink)
+    stop_name_departuretime_link = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopName.aspx?token=#{ENV['TRANSIT_API_KEY']}&agencyName=Caltrain&stopName=#{@stopname}"
+    stop_code_departuretime_link = "http://services.my511.org/Transit2.0/GetNextDeparturesByStopCode.aspx?token=#{ENV['TRANSIT_API_KEY']}&stopcode=#{@stopcode}"
+    @name_departure = HTTParty.get(stop_name_departuretime_link)
+    @code_departure = HTTParty.get(stop_code_departuretime_link)
   end
 
-  def get_stops
-      @departure#["RTT"]["AgencyList"]["Agency"]["RouteList"]["Route"].each_with_index do |hash, index|
+  def get_stops_by_name
+    @name_departure
 
-        # if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].nil?
-        #   @departuretime << "No departures within the next hour"
-        #   next
-        # else
-        #   if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].class == Array
-        #       hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].each do |time|
-        #         @departuretime << "Stop #{index + 1}: Minutes till next Departure: " + time
-        #       end
-        #     next
-        #   elsif hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].class == String
-        #       @departuretime << "Stop #{index + 1}: Minutes till next Departure: " + hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"]
-        #     next
-        #   else
-        #     @departuretime << "Stop #{index + 1}: No departures within the next hour"
-        #   end
-        # end
+  end
 
-        # return @departuretime.join("<br>")
-    # end #each
+  def get_stops_by_code
+      @departure["RTT"]["AgencyList"]["Agency"]["RouteList"]["Route"].each_with_index do |hash, index|
+
+        if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].nil?
+          @departuretime << "No departures within the next hour"
+          next
+        else
+          if hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"].class == Array
+              hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].each do |time|
+                @departuretime << "Stop #{index + 1}: Minutes till next Departure: " + time
+              end
+            next
+          elsif hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"].class == String
+              @departuretime << "Stop #{index + 1}: Minutes till next Departure: " + hash["RouteDirectionList"]["RouteDirection"]["StopList"]["Stop"]["DepartureTimeList"]["DepartureTime"]
+            next
+          else
+            @departuretime << "Stop #{index + 1}: No departures within the next hour"
+          end
+        end
+
+        return @departuretime.join("<br>")
+    end #each
   end #method
+
 end #class
