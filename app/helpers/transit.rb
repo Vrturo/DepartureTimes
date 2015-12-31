c39624e804e00f2f49d0d16806c49689d50718a3
Arturo Perez <VrturoPerez@gmail.com>
2015-12-30 23:05:47 -0800

    start conditional helper method that checks if next object is a hash or array

---
+++ app/helpers/transit.rb
@@ -0,0 +1,15 @@
+def conditional_method
+
+end
+
+# <RTT> {}
+#     <AgencyList> {}
+#         <Agency> {}
+#             <RouteList> {}
+#                 <Route> {} or []
+#                     <RouteDirectionList> {}
+#                         <RouteDirection {} or [] Name="SOUTHBOUND TO TAMIEN">
+#                             <StopList> {}
+#                                 <Stop {} name="College Park Caltrain Station">
+#                                     <DepartureTimeList /> nil or {}
+#                                       <DepartureTime> [] or ""
--- app/models/CalTrain.rb
+++ app/models/CalTrain.rb
@@ -104,17 +104,6 @@ class CalTrain
 
 end #class
 
-# <RTT> {}
-#     <AgencyList> {}
-#         <Agency> {}
-#             <RouteList> {}
-#                 <Route> {} or []
-#                     <RouteDirectionList> {}
-#                         <RouteDirection {} or [] Name="SOUTHBOUND TO TAMIEN">
-#                             <StopList> {}
-#                                 <Stop {} name="College Park Caltrain Station">
-#                                     <DepartureTimeList /> nil or {}
-#                                       <DepartureTime> [] or ""
 
 
 
