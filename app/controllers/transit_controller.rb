472ef9c0f5319c7197a4fdef32c8564fa20e76d2
Arturo Perez <VrturoPerez@gmail.com>
2016-01-02 15:22:01 -0800

    get unique array from controller to pass to mapjs file

--- app/controllers/transit_controller.rb
+++ app/controllers/transit_controller.rb
@@ -3,11 +3,15 @@ get '/' do
   @transit = CalTrain.new
   @transit.display_departures
 
+
+  # Making an array of just the stop names to the map.js file
   stop_name_arr = []
   @transit.stop_name.each do |stop_name|
     stop_name_arr << stop_name[:name]
   end
-  @stop_name_arr = stop_name_arr
+  #multiple stops cause stop names to come up more than once
+  # but we only need the stop name one time to display in map
+  @stop_name_arr = stop_name_arr.uniq!
 
   erb :index
 end
